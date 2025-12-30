ab1e35b34d3fe2aaf7729b3a9c6a3ed8f45b89a3
Saadkhan232423 <232423@students.au.edu.pk>
2025-12-30 16:34:46 +0500

    Added frontend project structure

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,23 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 't4.ftcdn.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4000',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: process.env.NODE_ENV === 'development',
+  },
 };
 
 export default nextConfig;
